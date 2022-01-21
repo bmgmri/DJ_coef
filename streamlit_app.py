bc292811dad575e9dbd2818c1723d573d893cd6c
bmgmri <98158095+bmgmri@users.noreply.github.com>
2022-01-21 14:08:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,54 +29,46 @@ if uploaded_file is not None:
 ##------------------------------------------------------------------
 ## FIT with initial shape  DATA 1 GAUSSIAN
 ##------------------------------------------------------------------
-
-def Gaussian(x, mean, h1 , sd ):
-    return  h1*(1/sd*np.sqrt(2*np.pi) ) * np.exp(-0.5*((x-mean)/sd)**2)  
+    def Gaussian(x, mean, h1 , sd ):
+        return  h1*(1/sd*np.sqrt(2*np.pi) ) * np.exp(-0.5*((x-mean)/sd)**2)  
 
 ## get x of max
-smoothed= savgol_filter(y, 31, 3)
-fig, ax = plt.subplots()
-ax.plot(x, smoothed,  lw=2, c='k')
+    smoothed= savgol_filter(y, 31, 3)
+    fig, ax = plt.subplots()
+    ax.plot(x, smoothed,  lw=2, c='k')
+    st.pyplot(fig)
+ 
+    ## filtramos el primer 20% de datos para buscar el maximo
+    shift= x.shape[0] // 5
+    #print(x.shape)
+    # altura y posicion del maximo
+    idmax = np.argmax(smoothed[shift:])
+    ymax = np.amax(smoothed[shift:])
+    x0=x[shift+idmax]
 
-'''
-idmax = np.argmax(smoothed)
-ymax = np.amax(smoothed)
-x0=x[idmax]
-colax=x[idmax:]
-colay=y[idmax:]
+    # separamos la curva a partir del maximo
+    colax=x[shift+idmax:]
+    colay=y[shift+idmax:]
 
-'''
-## filtramos el primer 20% de datos para buscar el maximo
-shift= x.shape[0] // 5
-#print(x.shape)
-# altura y posicion del maximo
-idmax = np.argmax(smoothed[shift:])
-ymax = np.amax(smoothed[shift:])
-x0=x[shift+idmax]
-
-# separamos la curva a partir del maximo
-colax=x[shift+idmax:]
-colay=y[shift+idmax:]
-
-#print(idmax, x0,ymax)
+    #print(idmax, x0,ymax)
 
 
-guess = [x0, ymax, x[-1]/10 ]
-popt , pcov  = curve_fit(  Gaussian, colax, colay, p0=guess , bounds=([x0, 0, 0], np.inf))
+    guess = [x0, ymax, x[-1]/10 ]
+    popt , pcov  = curve_fit(  Gaussian, colax, colay, p0=guess , bounds=([x0, 0, 0], np.inf))
 
-fig, ax = plt.subplots()
-ax.plot(colax, colay,  lw=2, c='k')
-ax.plot(colax , Gaussian(colax, *popt), label='0 DFO', lw=2, c='r')
-plt.savefig('curves.png', dpi=200 )
-st.pyplot(fig)
+    fig, ax = plt.subplots()
+    ax.plot(colax, colay,  lw=2, c='k')
+    ax.plot(colax , Gaussian(colax, *popt), label='0 DFO', lw=2, c='r')
+    plt.savefig('curves.png', dpi=200 )
+    st.pyplot(fig)
 
-fig, ax = plt.subplots()
-#ax.plot(x, y,  lw=2, c='k')
-ax.fill_between(x, y, 0, alpha=0.7)
-ax.plot(x , Gaussian(x, *popt), label='0 DFO', lw=2, c='r')
-#ax.plot(x , y - Gaussian(x, *popt), label='0 DFO', lw=2, c='k')
-plt.savefig('fit.png', dpi=200 )
-st.pyplot(fig) 
+    fig, ax = plt.subplots()
+    #ax.plot(x, y,  lw=2, c='k')
+    ax.fill_between(x, y, 0, alpha=0.7)
+    ax.plot(x , Gaussian(x, *popt), label='0 DFO', lw=2, c='r')
+    #ax.plot(x , y - Gaussian(x, *popt), label='0 DFO', lw=2, c='k')
+    plt.savefig('fit.png', dpi=200 )
+    st.pyplot(fig) 
 
 '''
 print()
